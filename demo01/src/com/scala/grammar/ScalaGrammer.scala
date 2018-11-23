d94e47acc9319dc137472e70a430122de59057bf
sunbo <sunbo930516@foxmail.com>
2018-11-23 13:56:10 +0800

    scala demo01
    Change-Id: I5eb6e8de1a5a56302c5b80672f8dcd2f10cd4816

--- demo01/src/com/scala/grammar/ScalaGrammer.scala
+++ demo01/src/com/scala/grammar/ScalaGrammer.scala
@@ -5,6 +5,9 @@ object ScalaGrammar {
     // 开启scala学习之旅
     def main(args: Array[String]): Unit = {
         println("开启Scala学习之旅!")
+        // 默认值输出
+        val students = new Students()
+        println(students.name)
     }
 
 }
---
+++ demo01/src/com/scala/grammar/Students.scala
@@ -0,0 +1,11 @@
+package com.scala.grammar
+
+/**
+  * 在scala中的祖先类是Any
+  * Any其中包含了AnyVal和AnyRef两部分构成的
+  */
+class Students {
+    // 在scala中_表示对应类型的默认值，比如String为null
+    var name: String = _
+
+}
