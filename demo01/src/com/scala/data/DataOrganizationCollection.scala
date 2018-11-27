de5d04f68ac795aa0be2b178b522ffdcce9783a6
sunbo <sunbo930516@foxmail.com>
2018-11-27 10:44:20 +0800

    集合中常用的方法
    Change-Id: Ia1180049fc292af3b8ca9f229e35a1c02835ea79

--- demo01/src/com/scala/data/DataOrganizationCollection.scala
+++ demo01/src/com/scala/data/DataOrganizationCollection.scala
@@ -12,7 +12,38 @@ package com.scala.data
   *
   * Seq序列：包含head和tail两部分内容，除了head其他的都是tail（除了头都是尾）
   *
+  * 方法的调用与java中的调用规则有些差异
   *
+  * 集合当中常用的方法：
+  * 1、map
+  * 2、flatten
+  * 3、flatMap
+  * 4、filter
+  * 5、sortBy
+  * 6、sortWith
+  * 7、foldLeft
+  * 8、aggregate（reduceByKey）
+  * 9、zip
+  * 10、mkString
+  * 11、foreach
+  *
+  * 集合当中了解的方法
+  * 1、sorted
+  * 2、grouped
+  * 3、fold（折叠）
+  * 4、foldRight
+  * 5、reduce
+  * 6、reduceRight
+  * 7、reduce
+  * 8、reduceLeft
+  * 9、union
+  * 10、intersect（交集）
+  * 11、diff（差集）
+  * 12、head（取集合中的头部，只有一个值）
+  * 13、tail（取集合中的尾部，除了头部其余的都是尾部）
+  * 14、length
+  * 15、slice
+  * 16、sum
   */
 object DataOrganizationCollection {
 
