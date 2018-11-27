76add518b9562f086d386bdbc5d55d6bdb8f0d30
sunbo <sunbo930516@foxmail.com>
2018-11-27 09:18:09 +0800

    scala collection
    Change-Id: I4857311159475099f5ccf022b71fd62b0f5e6240

---
+++ demo01/src/com/scala/data/DataOrganizationCollection.scala
@@ -0,0 +1,19 @@
+package com.scala.data
+
+/**
+  * 在Scala中, 集合分为可变集合（mutable）和不可变集合（immutable）
+  *
+  * 可变集合指的是：长度可变内容可变
+  * 不可变集合指的是：长度不可变内容不可变
+  *
+  * Array： 内容都可变
+  *         长度可变数组ArrayBuffer
+  *         长度不可变数组Array
+  *
+  * Seq序列：包含head和tail两部分内容，除了head其他的都是tail（除了头都是尾）
+  *
+  *
+  */
+object DataOrganizationCollection {
+
+}
